40878b8b60f426e7115b390cf71300c398124be8
Mohamed Zaki <mohamed.ahmed-zaky@student.guc.edu.eg>
2019-02-20 17:03:45 +0100

    Some Fixes

--- src/player.py
+++ src/player.py
@@ -1,6 +1,7 @@
 import socket
 import queue
 import message
+from threading import Thread
 import utils
 
 
@@ -14,11 +15,7 @@ def main():
 
     meta_file = utils.Metainfo('file.libr')
     player_id = utils.generate_player_id()
-    
-    q = queue.Queue()
-    c = consumer(q)
-    c.daemon = True
-    c.start()
+
 
     listen_port = 7777
     my_details = dict()
@@ -48,8 +45,8 @@ def main():
         port = player[b'port']
 
         s = socket.create_connection((ip, port))
-        handshake_send = message.ComMessage.HandshakeMsg(my_details['player_id'],meta_file.get_info_hash()).msg_encode()
-        socket.sendall(handshake_send)
+        handshake_send = message.HandshakeMsg(my_details['player_id'],meta_file.get_info_hash()).msg_encode()
+        s.sendall(handshake_send)
         received_message = s.recv(1024)
         print(received_message)
         status, remain, objMsg = message.ComMessage.msg_decode(received_message)
@@ -72,8 +69,8 @@ def player_listen(q, my_details):
             msg = client_socket.recv(1024)
             status, remain, objMsg = message.ComMessage.msg_decode(msg)
             if objMsg.code is 0x00:
-                  handshake_send = message.ComMessage.HandshakeMsg(my_details['player_ip'], my_details['info_hash']).msg_encode()
-                  socket.sendall(handshake_send)  
+                  handshake_send = message.HandshakeMsg(my_details['player_ip'], my_details['info_hash']).msg_encode()
+                  client_socket.sendall(handshake_send)
             handle_player_listen(client_socket, q, my_details).start()
             handle_player_send(client_socket, q, my_details).start()
 
@@ -111,7 +108,7 @@ def handle_player_listen(socket, q, my_details):
                             
                         elif objMsg.message_code is 0x05: # Bitfield
                             other_bitfield = hex(objMsg.get_bitfield())
-                            for i in len(range(my_bitfield)):
+                            for i in range(my_bitfield):
                                 if my_bitfield[[i]] is 0 and other_bitfield[[i]] is 1: # TODO Apply algorithm here to select which index to request first
                                     request_message = message.ComMessage.RequestMsg(i).msg_encode()
                                     socket.sendall(request_message)    
@@ -160,151 +157,6 @@ def handle_player_send(socket, q, receiver):
         q.put()
 
     t = Thread(target=handle)
-    return t
-
-
-def consumer(q):
-    def consume():
-        while True:
-            c, i, by = q.get()
-            if c == "RENDER":
-                d.render_clean()
-            if c == "MOVE":
-                d.move_value_right(i, by)
-            if c == "ADD":
-                d.add_value(i, by)
-
-    t = Thread(target=consume)
-    return t
-
-
-if __name__== "__main__":
-  main()
-.generate_player_id()
-
-    q = queue.Queue()
-    c = consumer(q)
-    c.daemon = True
-    c.start()
-
-    listen_port = 7777
-    player_listen(q, listen_port).start()
-
-    #TODO Contact Hub for Players list and report peer_id & port
-
-    hub_socket = socket.create_connection((meta_file.get_hub_ip(), meta_file.get_hub_port()))
-    hub_msg = message.HubNotifyMsg(meta_file.get_info_hash(), player_id, listen_port, 0x8000, 0x4000, 65255558, b'start')\
-        .msg_encode()
-
-    hub_socket.sendall(hub_msg)
-
-    hub_recv = hub_socket.recv(1024)
-    status, remain, hub_recv = message.ComMessage.msg_decode(hub_recv)
-
-    players = hub_recv.players
-
-    #TODO connect and Handshake per player
-
-    for player in players:
-        ip = player[b'ip']
-        port = player[b'port']
-
-        s = socket.create_connection((ip, port))
-        handle_player_listen(s, q, False).start()
-        handle_player_send(s, q, False).start()
-
-
-def player_listen(q, port):
-    def handle():
-        server_socket_no = port
-        server_socket = socket.socket()
-        server_socket.bind(('localhost', server_socket_no))
-        server_socket.listen()
-
-        while True:
-            print("Waiting for connection")
-            client_socket, addr = server_socket.accept()
-            handle_player_listen(client_socket, q, True).start()
-            handle_player_send(client_socket, q, True).start()
-
-    t = Thread(target=handle)
-    return t
-
-
-def handle_player_listen(socket, q, receiver):
-    def handle():
-        print("Received connection", socket)
-
-        if receiver:
-            #TODO wait for handshake here (RECEIVING)
-            pass
-
-        while True:
-            try:
-                message = socket.recv(1)
-                # Decode the message
-                if message is not None:
-                    # Handle different messages
-                    if message == 'magic_move':
-                        # q.put()
-                        pass
-                    else:
-                        print(message)
-                else:
-                    print("Client disconnected", socket)
-                    break
-            except:
-                print("Client disconnected", socket)
-                break
-        q.put()
-
-    t = Thread(target=handle)
-    return t
-
-
-def handle_player_send(socket, q, receiver):
-    def handle():
-        print("Received connection", socket)
-
-        if not receiver:
-            #TODO Initiate handshake here (SENDER)
-            pass
-
-        while True:
-            try:
-                message = socket.recv(1)
-                #TODO Decode the message
-                if message is not None:
-                    #TODO Handle different messages
-                    if message == 'magic_move':
-                        # q.put()
-                        pass
-                    else:
-                        print(message)
-                else:
-                    print("Client disconnected", socket)
-                    break
-            except:
-                print("Client disconnected", socket)
-                break
-        q.put()
-
-    t = Thread(target=handle)
-    return t
-
-
-def consumer(q):
-    def consume():
-        while True:
-            c, i, by = q.get()
-            if c == "RENDER":
-                d.render_clean()
-            if c == "MOVE":
-                d.move_value_right(i, by)
-            if c == "ADD":
-                d.add_value(i, by)
-
-    t = Thread(target=consume)
     return t
 
 
