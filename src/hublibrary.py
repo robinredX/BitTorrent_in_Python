8363bba32d1c9f84275ccfa0f1008f0966a57e12
LaetitiaCouge <44775082+LaetitiaCouge@users.noreply.github.com>
2019-02-24 17:38:15 +0100

    Add specific thread and queue to HubLibrary

--- src/hublibrary.py
+++ src/hublibrary.py
@@ -10,13 +10,72 @@ import queue
 import os
 from netutils import read_line
 import numpy as np
+from enum import Enum
 
 import message
 import utils
+from utils import LibQMsgEnum, HubQMsgEnum
+
+
 
 class HubLibrary(object):
+    """ Manage access to the hub library which deals with the list of players
+    The list of players is access through a queue to manage multiple access from different player connections
+    """
+
     def __init__(self):
         self.lib = {}
+        self.q = queue.Queue()
+        t = self.consumer()
+        t.daemon = True
+        t.start()
+        
+ 
+    def consumer(self):
+        """ Watch requests arriving in the queue to add, remove players and get player list
+        """
+        def consume():
+            while True:
+                qmsg_id, info = self.q.get()
+                
+                if qmsg_id.value == LibQMsgEnum.MSGQ_ADD_PLAYER_LIST.value:
+                    #print('Library has received a message to add player')
+                    client_queue, param, socket, addr, msg = info
+                    status, remain, objMsg = message.ComMessage.msg_decode(msg)
+                    if status == 0:
+                        msg_type = objMsg.get_message_type()            
+                        if msg_type == 'hub notify':
+                            self.update_dictionary_info(objMsg, addr)
+                            lib_ID = objMsg.get_info_hash()
+                            player_ID = objMsg.get_player_id()
+                            player_list, seeder_number, leecher_number = self.get_player_list(lib_ID, player_ID, addr, 50)
+                            
+                            objMsg = message.HubAnswerMsg(b'', b'', 100, 5, seeder_number, leecher_number, player_list)
+                            msg = objMsg.msg_encode()
+                            client_queue.put((param,(socket, addr, msg)))
+
+                if qmsg_id.value == LibQMsgEnum.MSGQ_REMOVE_PLAYER_LIST.value:
+                    #print('Library has received a message to remove players')    
+                    list_to_remove = {}                    
+                    for cnx in info:
+                        ip, port = cnx
+                        for file_hash in self.lib.keys():
+                            for player_id in self.lib[file_hash].keys():
+                                if self.lib[file_hash][player_id]['ip'] == ip and self.lib[file_hash][player_id]['port'] == port:
+                                    if file_hash not in list_to_remove.keys():
+                                        list_to_remove[file_hash] = []                                     
+                                    list_to_remove[file_hash].append(player_id)    
+                                                        
+                    for file_hash in list_to_remove.keys():
+                        self.remove_player_list(file_hash, list_to_remove[file_hash])
+                        
+        t = Thread(target=consume)
+        return t  
+        
+        
+    def get_library_queue(self):
+        return(self.q)
+        
 
     def update_dictionary_info(self, decoded_msg, address):
         lib_file = decoded_msg.get_info_hash()
@@ -29,52 +88,55 @@ class HubLibrary(object):
         requires_reply = 0
 
         if event == b'start':
-            print("EVENT: start")
+            #print("EVENT: start")
             try:
-                self.lib[lib_file][player] = {"ip": address[0], "port": address[1], "complete": int(0 == left)}
+                self.lib[lib_file][player] = {"ip": address[0], "port": port, "complete": int(0 == left)}
             except:
                 self.lib[lib_file] = {}
-                self.lib[lib_file][player] = {"ip": address[0], "port": address[1], "complete": int(0 == left)}
+                self.lib[lib_file][player] = {"ip": address[0], "port": port, "complete": int(0 == left)}
             requires_reply = 1
 
         elif event == b'stopped':
-            print("EVENT: stopped")
+            #print("EVENT: stopped")
             try:
                 del self.lib[lib_file][player]
             except:
                 print("IGNORED: no such library/player")
         elif event == b'completed':
-            print("EVENT: completed")
+            #print("EVENT: completed")
             if 0 == left:
                 self.lib[lib_file][player]["seed"] = {int(0 == left)}
 
         else:
-            print("ERROR: something is wrong!")
-        print(self.lib)
+            print("something is wrong!")
+        #print(self.lib)
         return requires_reply
 
-    def remove_player_list(self, lib_file, players):
+    def remove_player_list(self, lib_file, player_list):
         #TODO consider removing from all lib_files, since connection is down
-        for player in players:
+        for player_id in player_list:
             try:
-                del self.lib[lib_file][player]
+                del self.lib[lib_file][player_id]
             except:
                 print("IGNORED: no such library/player")
+            
 
-    def send_player_list(self, socket, lib_file, address, number_requested = 50):
-        lib_ID = lib_file.get_info_hash()
-        seeder_number = 1
-        leecher_number = 1
+    def get_player_list(self, lib_ID, player_ID, address, number_requested = 50):
+        seeder_number = 0
+        leecher_number = 0
+        
         players = np.random.choice(list(self.lib[lib_ID].keys()),size = min(len(self.lib[lib_ID]), number_requested), replace = False)
+        players = np.ndarray.tolist(players)
+        players.remove(player_ID)
+        #print(players)        
         to_send = []
+        
         for player in players:
             to_send += {'player_id': player, 'ip':self.lib[lib_ID][player]["ip"], 'port':self.lib[lib_ID][player]["port"], 'complete':self.lib[lib_ID][player]["complete"]},
-        if len(players)!=0:
-            msg = message.HubAnswerMsg(b'', b'', 100, 5, seeder_number, leecher_number, to_send).msg_encode()
-            print(socket.sendall(msg+bytes("\n", 'utf-8')))
-        else:
-            print("no players")
-        # TODO: player should not be served in the list of players
-        # TODO: warnings should be about player availability, seeder availability...?
-        # TODO: seeder and leecher number should come from the dictionary
-   
+            if self.lib[lib_ID][player]['complete'] == 1:
+                seeder_number += 1
+            else:
+                leecher_number += 1
+
+        return to_send, seeder_number, leecher_number
+        
