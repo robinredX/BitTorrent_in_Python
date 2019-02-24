357e05ba22ee2612111c5ba5803a04ee29adf16d
LaetitiaCouge <44775082+LaetitiaCouge@users.noreply.github.com>
2019-02-24 17:47:46 +0100

    Manage player connections to hub

--- src/hub.py
+++ src/hub.py
@@ -1,18 +1,36 @@
+import sys, socket
 from queue import Queue
-import hubplayer
-import hublibrary
+from hubplayer import HubPlayer
+from hublibrary import HubLibrary 
 
 def main():
     this_IP = 'localhost'
-    at_this_port = 7777
+    at_this_port = 8001
     hub_address = (this_IP, at_this_port)
-
-    queue =  Queue()
-    hub_library = hublibrary.HubLibrary()
-    hub_to_player = hubplayer.HubPlayer(hub_address)
     
-    hub_to_player.listen(queue).start()
-    hub_to_player.speak(queue, hub_library).start()
-
+    hub_library = HubLibrary()
+    lib_queue = hub_library.get_library_queue()
+    
+    # Start server socket allowing player connection
+    while True:
+        try :
+            server_socket = socket.socket()
+            server_socket.bind((this_IP, at_this_port))
+            server_socket.listen() 
+            break
+        except:
+            print('Cannot connect hub. Terminate.')
+            sys.exit(-1)
+            
+                
+    print('Hub is connected')
+    print("Waiting for connection")    
+    # Wait for player connection
+    while True:       
+        socket_client, addr = server_socket.accept()   
+        print("Received connection", socket_client)            
+        client = HubPlayer(socket_client, addr, lib_queue)    
+    
+    
 if __name__== "__main__":
   main()
